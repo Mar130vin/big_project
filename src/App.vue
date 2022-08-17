2c1952bf77aaece07418d48fa4911515aa91f0bc
杨孟帆130 <1206158637@qq.com>
2022-08-17 09:10:12 +0800

    项目模板, 自定义初始化完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
     123
+
   </div>
 </template>
 
 <script>
+import { getListAPI } from '@/api'
 export default {
+  created () {
+    this.getListFn()
+  },
+  methods: {
+    async getListFn () {
+      const res = await getListAPI()
+      console.log(res)
+    }
+  }
 }
 </script>
 
